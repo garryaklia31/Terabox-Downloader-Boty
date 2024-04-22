b5572b4cdbbbac4775c10a30eec66552497e2cb2
garryaklia31 <158801619+garryaklia31@users.noreply.github.com>
2024-04-22 16:05:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ API_HASH = "a0057fbf1ca49ce5e9d26fd4afd6e78b"  # api hash
 BOT_TOKEN = "6819695620:AAHpNgX852w0MktEN8fFGWd4RzW3PtMoOTY"  # bot token
 
 ## REDIS
-REDIS_HOST = "redis-19765.c267.us-east-1-4.ec2.cloud.redislabs.com:19765"  # redis host uri
-REDIS_PORT = 6379  # redis port
-REDIS_PASSWORD = "Mehakpreet@572"  # redis password
+REDIS_HOST = "redis-16448.c73.us-east-1-2.ec2.cloud.redislabs.com"  # redis host uri
+REDIS_PORT = 16448  # redis port
+REDIS_PASSWORD = "o5BMtARZTkrMHP7xvxkOEx6XrXS583NN"  # redis password
 
 PRIVATE_CHAT_ID = -1001757174395  # CHAT WHERE YOU WANT TO STORE VIDEOS
 ADMINS = [1718738592]
