db882fbe4ba5a00a8aaa0d8918963316d1c348a7
garryaklia31 <158801619+garryaklia31@users.noreply.github.com>
2024-04-22 15:09:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,23 @@
 from os import environ
 
 
-# BOT CONFIG
-API_ID = environ.get("API_ID", 123456)  # api id
-API_HASH = environ.get("API_HASH", "")  # api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "")  # bot token
+API_ID = 28386099  # api id
+API_HASH = "a0057fbf1ca49ce5e9d26fd4afd6e78b"  # api hash
 
-# REDIS
-REDIS_HOST = environ.get("REDIS_HOST", "localhost")  # redis host uri
-REDIS_PORT = environ.get("REDIS_PORT", 12345)  # redis port
-REDIS_PASSWORD = environ.get(
-    "REDIS_PASSWORD", "q8vcRgGfoZgwKT6irvJ6AixPs1lFZdW8"
-)  # redis password
+BOT_TOKEN = "6819695620:AAHpNgX852w0MktEN8fFGWd4RzW3PtMoOTY"  # bot token
 
 
-ADMINS = [6791744215]
-OWNER_ID = 6791744215  # Replace with your Telegram user ID
-PRIVATE_CHAT_ID = -100  # CHAT WHERE YOU WANT TO STORE VIDEOS
-USER_CHANNEL = -100
-DUMP_CHANNEL = -100
+## REDIS
+HOST = "redis-16448.c73.us-east-1-2.ec2.cloud.redislabs.com"  # redis host uri
+PORT = 16448  # redis port
+PASSWORD = "o5BMtARZTkrMHP7xvxkOEx6XrXS583NN"  # redis password
+
+PRIVATE_CHAT_ID = -1001757174395  # CHAT WHERE YOU WANT TO STORE VIDEOS
+ADMINS = [1718738592]
+
+COOKIE = "PANWEB=1; TSID=m0RHbkAjLAp2ugYpZoIp9wgg7FHQY095; __bid_n=18be85f750d4f42dba4207; _ga=GA1.1.1627430852.1700410655; fbm_756701921753953=base_domain=.terabox.app; ndus=Y-v8LeYteHuiTHDIo5RLqixbQFofA06gObCb9DUE; __stripe_mid=ef178580-35ba-47ab-a706-1a000765950394f456; browserid=M8oIfy3G85e0v99eknqJDPeek9TdfnX963_PtXYvuosk_bIUUWm-W7sFEng=; lang=en; _ga_06ZNKL8C2E=GS1.1.1707915442.11.0.1707915554.60.0.0; csrfToken=EESlvx4P7Edqh6tOG30jTr7g"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (dont use this)
+
+DUMP_CHANNEL = 1001757174395
 
 
-# Config
-COOKIE = environ.get("COOKIE", "")
+
